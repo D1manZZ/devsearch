a763eb0cadff4df84b57d6f274f74765ae0058a7
Dima Kuriptya <dima343782@gmail.com>
2021-10-26 08:05:43 +0300

    add whitenoise

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'devsearch.urls'
@@ -135,6 +136,7 @@ STATICFILES_DIRS = [
 ]
 MEDIA_ROOT = BASE_DIR.joinpath('media')
 STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
